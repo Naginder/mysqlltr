4fd9cf800c8f2d0c14f1c53a1f2695df194e7c10
naginder <naginder@hotmail.com>
2021-12-30 12:59:02 +0800

    added logs in runbook

--- template/runbook/backupmysql.ps1
+++ template/runbook/backupmysql.ps1
@@ -12,7 +12,7 @@ Param(
     [Parameter(Mandatory = $true)]
     [String] $storagename,
     [Parameter(Mandatory = $true)]
-    [String] $backupfileshare   
+    [String] $backupfileshare  
 )
 
 # Ensures you do not inherit an AzContext in your runbook
@@ -40,13 +40,36 @@ $volume=New-AzContainerGroupVolumeObject -Name "backups" -AzureFileShareName $ba
         -AzureFileStorageAccountKey (ConvertTo-SecureString $storagekey -AsPlainText -Force)
 #create container object
 $container = New-AzContainerInstanceObject -Name mysqldumpci1 -Image schnitzler/mysqldump -VolumeMount $volumemount `
-            -Command "mysqldump","--opt","--single-transaction",$hostname,$username,$password,$filename,"--all-databases"
+            -Command "mysqldump","--opt","--single-transaction",--host=$hostname,--user=$username,--password=$password,$filename,"--all-databases","--force"
 #deploy the container in azure container groups
 Write-Output "creating container"
 $containergroup=New-AzContainerGroup -ResourceGroupName $rgname -Name mysqldumpci1  -Location eastus -Container $container -Volume $volume `
             -RestartPolicy Never -OSType Linux 
+
+while ($true)
+{
+	$status=(get-azcontainergroup -name mysqldumpci1 -resourcegroupname rgtest1 | select-object -property @{name="Status";expression={$_.InstanceViewState}}).Status
+	if ($status -eq "Failed")
+	{
+		Write-Output "Container in Failed State, Please check the logs below"
+		Break
+	}
+	elseif(($status -eq "Stopped")  -or ($status -eq "Succeeded"))
+	{
+		Write-Output "Container execution is done, Please check the logs below"
+		Break
+	}
+	else
+	{
+		Write-Output $status
+	}
+}
+
+Get-AzContainerInstanceLog -ContainerGroupName mysqldumpci1 -ContainerName mysqldumpci1 -ResourceGroupName $rgname | Write-Output
+
 #stop container after backup
 Write-Output "stopping container"
 Stop-AzContainerGroup -Name mysqldumpci1 -ResourceGroupName $rgname
 
 #remove container
+
